51bdcb34940295406fba170219608bbb18aa1492
cXyn-h <xynthanium@gmail.com>
2023-11-21 00:05:42 -0800

    remove node index debugging print statements

--- src/DialogNodes/CacheNodeIndex.py
+++ src/DialogNodes/CacheNodeIndex.py
@@ -9,17 +9,13 @@ class CacheNodeIndex(AbstractIndex):
         if indexing_field not in ["event", "session", "status"]:
             raise Exception(f"Index for active Nodes stored in Dialog handler given a field name that it does not support indexing on. must be 'event', 'session', or 'status'")
         self.indexing_field = indexing_field
-        print(self.indexing_field)
 
     def parse_for_keys(self, entry: AbstractCacheEntry):
-        print("parsing for keys beginning", self.indexing_field)
         if not issubclass(entry.__class__, BaseType.BaseNode):
             return []
-        print("parsing for keys mid", self.indexing_field)
         if self.indexing_field == "session":
             return [id(entry.session)]
         elif self.indexing_field == "event":
-            print(list(entry.graph_node.get_events().keys()))
             return list(entry.graph_node.get_events().keys())
         elif self.indexing_field == "status":
             return [entry.status]
