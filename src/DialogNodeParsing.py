fd87cc658222226795f8a7ddb68bd2efae1b4a3e
cXyn-h <xynthanium@gmail.com>
2024-09-17 18:54:14 -0700

    QOL updates to parsing, comments
    * cleanup comments and type annotations
    + add function for parsing string

--- src/DialogNodeParsing.py
+++ src/DialogNodeParsing.py
@@ -3,29 +3,30 @@ import copy
 import inspect
 import logging
 
+# known node type to parse to
 import src.DialogNodes.BaseType as BaseType
+# pretty log messages and other tools
 import src.utils.LoggingHelper as logHelper
 
-# for validing yaml in right format. why *JSON*schema? because it validates based on read in dictionaries. Once yaml is read in there's no difference
+# for validing yaml has right format. 
+# why *JSON*schema? because it validates based on already read in dictionaries. Once yaml is read in there's no difference
 from jsonschema import validate, ValidationError
-
+import types
 #TODO: future: autoimport nodes from folder?
-#TODO: future QoL: allowing node types to extend from non Base type nodes
 
 parsing_logger = logging.getLogger('Dialog Parser')
 logHelper.use_default_setup(parsing_logger)
 parsing_logger.setLevel(logging.INFO)
 
-#TODO: this allows globally, any instances of needing to limit to subsets? -ie would this need to be in a class
 ALLOWED_NODE_TYPES = {}
 '''The global cache of what types of nodes the parser can handle. dinctionary of node type name to node type module.
     that single module should contain both GraphNode and active Node of the type'''
 #~~~~~~~~~ FURTHER SETUP OF THESE ARE DONE AFTER DEFINING FUNCTIONS FOR HANDLING AND REGESTERING ~~~~~~~~~
 
-def find_node_classes(type_module):
+def find_node_classes(type_module:types.ModuleType):
     '''
     finds node types that have both classes defined within a module, not necessarily fully valid node type definition. 
-    note that while technically you can have more than one type's classes in a file,
+    note that while technically you can have more than one Node type's classes in a file,
     its not recommended for organization sake. Module must have both GraphNode and active Node classes of a type defined to support the type.
 
     Parameters
@@ -35,7 +36,7 @@ def find_node_classes(type_module):
     
     Return
     ---
-    a list of types that hoave both classes defined within module. Those still may not be valid nodes'''
+    a list of type names that have both classes defined within module. Those still may not be valid nodes'''
     found_graph_node_types = []
     found_node_types = []
     for name, obj in inspect.getmembers(type_module):
@@ -47,12 +48,14 @@ def find_node_classes(type_module):
 
     supported_types = []
     # type considered supported if both GraphNode and active Node classes are present
+    # looping through one list is enough because looking for things in both. everything that could pass needs to be in
+    #   list that we randomly pick to be the one to loop through
     for type in found_graph_node_types:
         if type in found_node_types:
             supported_types.append(type)
     return supported_types
 
-def register_node_type(type_module, type_name:str, re_register=False, allowed_types:"dict[str,BaseType.BaseGraphNode]"=ALLOWED_NODE_TYPES):
+def register_node_type(type_module, type_name:str, re_register=False, allowed_types:"dict[str, types.ModuleType]"=ALLOWED_NODE_TYPES):
     '''validates and if passes validation registers the given node type so the parser can use it. Raises Exceptions for invalid node type
 
     Parameters
@@ -60,20 +63,25 @@ def register_node_type(type_module, type_name:str, re_register=False, allowed_ty
     type_module - `module`
         module that contains the GraphNode and Node classes for the type
     type_name - `str`
-        name of the type, should match the TYPE in the GraphNode class and the name of the class
+        name of the type, should match the TYPE field in the GraphNode class and the name of the class
     re-register - `bool`
         boolean for whether or not it's ok to override if already registered. prints out warning if trying to re-register and this is false
     allowed_types - `dict[str, module]`
         dictonary of types that are already registered and allowed to use for parsing. uses global storage dictionary as default, otherwise can override
         with local registries
+
+    Raises
+    ---
+    Exception if type fails validation
         
     Returns
     ---
     `bool` - wether or not type was registered or re-registered'''
+
     parsing_logger.debug(f"trying to register a node under <{type_name}>")
-    graph_node_ref:BaseType.BaseGraphNode = getattr(type_module, type_name+"GraphNode")
     # if not valid, it will error out and won't finish registering
     validate_type(type_module, type_name)
+    graph_node_ref:BaseType.BaseGraphNode = getattr(type_module, type_name+"GraphNode")
 
     if graph_node_ref.TYPE in allowed_types and not re_register:
         parsing_logger.warning(f"type <{graph_node_ref.TYPE}> already registered, and not allowed to re-register. skipping")
@@ -84,7 +92,7 @@ def register_node_type(type_module, type_name:str, re_register=False, allowed_ty
         parsing_logger.debug(f"node type <{graph_node_ref.TYPE}> registerd, full list now is <{allowed_types}>")
         return True
 
-def validate_type(type_module, type_name:str):
+def validate_type(type_module:types.ModuleType, type_name:str):
     '''validates node type is formatted correctly. module must have two classes named: [type]GraphNode and [type]Node where you replace [type] with
     actual name of the type. GraphNode must have TYPE field, and be able to fetch schema, fields and have a compatible version. 
     best to extend from BaseType. Raises Exception if type is not formatted correctly.
@@ -95,13 +103,15 @@ def validate_type(type_module, type_name:str):
         module that contains the Node classes for the type
     type_name - `str`
         name of the type, should match the TYPE in the GraphNode class
+
+    Raises
+    ---
+    Exception if badly formatted type - missing classes, TYPE field mismatch, failed to fetch node fields
     '''
     if not hasattr(type_module, type_name+"GraphNode") or not inspect.isclass(getattr(type_module, type_name+"GraphNode")):
         raise Exception(f"cannot find a <{type_name}GraphNode> class in node type <{type_name}> module.")
     if not hasattr(type_module, type_name+"Node") or not inspect.isclass(getattr(type_module, type_name+"Node")):
         raise Exception(f"cannot find a <{type_name}Node> class in node type <{type_name}> module.")
-
-    #TODO: validate duck typing: all needed methods are there? lots of methods though
 
     graph_node:BaseType.BaseGraphNode = getattr(type_module, type_name+"GraphNode")
     graph_node.clear_caches()
@@ -126,7 +136,7 @@ def validate_type(type_module, type_name:str):
     graph_node.get_node_schema()
     #TODO: maybe use some of JSONSchema meta schema stuff to validate schema is usable by system? not sure how yet
 
-def empty_cache(allowed_types:"dict[str,BaseType.BaseGraphNode]"=ALLOWED_NODE_TYPES):
+def empty_cache(allowed_types:"dict[str,types.ModuleType]"=ALLOWED_NODE_TYPES):
     '''empties cache for different node types' definitions and schemas. Nodes may store some cached data as well so clears node caches of the node types
     passed in as well'''
     for node_type, node_module in allowed_types.items():
@@ -142,7 +152,7 @@ def parse_name(name):
     #TODO: future QoL WIP, potentially for names with . in them
     pass
 
-def parse_files(*file_names:str, existing_nodes:dict = None, allowed_types:"dict[str,BaseType.BaseGraphNode]"=ALLOWED_NODE_TYPES):
+def parse_files(*file_names:str, existing_nodes:dict = None, allowed_types:"dict[str,types.ModuleType]"=ALLOWED_NODE_TYPES):
     '''same idea as `parse_file` parses all the passed in files for nodes and appends them onto existing nodes. Can handle multiple yaml documents
     in files. Raises error if GraphNode already found or GraphNode definition badly formatted
 
@@ -166,7 +176,7 @@ def parse_files(*file_names:str, existing_nodes:dict = None, allowed_types:"dict
         parse_file(file_name, existing_nodes, allowed_types=allowed_types)
     return existing_nodes
 
-def parse_file(file_name, existing_nodes:dict=None, allowed_types:"dict[str,BaseType.BaseGraphNode]"=ALLOWED_NODE_TYPES):
+def parse_file(file_name, existing_nodes:dict=None, allowed_types:"dict[str,types.ModuleType]"=ALLOWED_NODE_TYPES):
     '''parses a file for nodes and appends them onto existing nodes. Can handle multiple yaml documents
     in files. Raises error if already there or graph node definition badly formatted
 
@@ -186,29 +196,32 @@ def parse_file(file_name, existing_nodes:dict=None, allowed_types:"dict[str,Base
         same format as passed in existing nodes: node id to GraphNode object. list of existing nodes with ones just parsed from file added'''
     # don't want truthy comparison so user can pass in a dict object they want filled
     existing_nodes = existing_nodes if existing_nodes is not None else {}
-    num_added = 0
     with open(file_name) as file:
-        parsing_logger.debug(f"loading file <{file_name}>, with existing nodes <{existing_nodes}>")
-        doc_dict = yaml.safe_load_all(file)
-        for doc_ind, yaml_doc in enumerate(doc_dict):
-            parsing_logger.debug(f"parsing document indexed <{doc_ind}> from file <{file_name}>")
-            if yaml_doc is None or "nodes" not in yaml_doc or yaml_doc["nodes"] is None or len(yaml_doc["nodes"]) == 0:
-                parsing_logger.info(f"file {file_name} document indexed {doc_ind} does not have list of nodes, moving to next YAML document")
-                continue
-
-            for node_ind, yaml_node in enumerate(yaml_doc["nodes"]):
-                node = parse_node(yaml_node, file_location=f"file <{file_name}> doc <{doc_ind}> node <{node_ind}>", allowed_types=allowed_types)
-                if node.id in existing_nodes:
-                    # technically could ignore second, but better to tell whoever wrote it so any differences don't cause confusion of why misbehaving
-                    raise Exception(f"Exception at file {file_name} doc {doc_ind} node {node_ind}, node <{node.id}> is already loaded, can't accept the second definition")
-                num_added += 1
-                existing_nodes[node.id] = node
-                parsing_logger.debug(f"existing nodes are now <{existing_nodes}>")
-
-    parsing_logger.info(f"finished loading file <{file_name}>, found <{num_added}> nodes from file")
+        parse_contents(file, file_name, existing_nodes=existing_nodes, allowed_types=allowed_types)
+    parsing_logger.info(f"finished loading file <{file_name}>")
     return existing_nodes
 
-def parse_node(yaml_node, file_location="", allowed_types:"dict[str,BaseType.BaseGraphNode]"=ALLOWED_NODE_TYPES):
+def parse_contents(content_string, file_location = "", existing_nodes:dict=None, allowed_types:"dict[str,types.ModuleType]"=ALLOWED_NODE_TYPES):
+    existing_nodes = existing_nodes if existing_nodes is not None else {}
+    file_location = 'file: ' + file_location if file_location else 'string'
+    parsing_logger.debug(f"loading content from <{file_location}>, with existing nodes <{existing_nodes}>")
+    doc_dict = yaml.safe_load_all(content_string)
+    for doc_ind, yaml_doc in enumerate(doc_dict):
+        parsing_logger.debug(f"parsing document indexed <{doc_ind}> from <{file_location}>")
+        if yaml_doc is None or "nodes" not in yaml_doc or yaml_doc["nodes"] is None or len(yaml_doc["nodes"]) == 0:
+            parsing_logger.info(f"parsed from {file_location} document indexed {doc_ind} does not have list of nodes, moving to next YAML document")
+            continue
+
+        for node_ind, yaml_node in enumerate(yaml_doc["nodes"]):
+            node = parse_node(yaml_node, file_location=f"location <{file_location}> doc <{doc_ind}> node <{node_ind}>", allowed_types=allowed_types)
+            if node.id in existing_nodes:
+                # technically could ignore second, but better to tell whoever wrote it so any differences don't cause confusion of why misbehaving
+                raise Exception(f"Exception in {file_location} doc {doc_ind} node {node_ind}, node <{node.id}> is already loaded, can't accept the second definition")
+            parsing_logger.debug(f"added node <{node.id}>")
+            existing_nodes[node.id] = node
+    return existing_nodes
+
+def parse_node(yaml_node, file_location="", allowed_types:"dict[str,types.ModuleType]"=ALLOWED_NODE_TYPES):
     '''function that takes yaml for one node and validates and creates a GraphNode object of the type specified in yaml.
     raises errors for anything wrong in node definition
 
@@ -216,7 +229,7 @@ def parse_node(yaml_node, file_location="", allowed_types:"dict[str,BaseType.Bas
     ---
     yaml_node - `dict`
         loaded yaml object that holds the node's settings
-    location_printout - `str`
+    file_location - `str`
         extra information to print out in logging or exceptions to help with finding where in the yaml file the error came from
 
     Return
@@ -234,7 +247,7 @@ def parse_node(yaml_node, file_location="", allowed_types:"dict[str,BaseType.Bas
     parsing_logger.debug(f"parsed node is <{graph_node.id}>")
     return graph_node
 
-def validate_yaml_node(yaml_node, file_location="", allowed_types:"dict[str,BaseType.BaseGraphNode]"=ALLOWED_NODE_TYPES):
+def validate_yaml_node(yaml_node, file_location="", allowed_types:"dict[str,types.ModuleType]"=ALLOWED_NODE_TYPES):
     '''validates parsed yaml correctly defines a node. checks type is allowed, version is close enough to Class definition's version, and validates using schema.
     raises errors if node type is not allowed, versions are too far, bad node type definitions, or any other bad formatting in yaml
 
@@ -274,9 +287,12 @@ def validate_yaml_node(yaml_node, file_location="", allowed_types:"dict[str,Base
         node_version = yaml_node["v"]
 
     if not node_version:
+        # assume is latest version and try that. error later.
+        # if version is required, that is on the node to specify
         parsing_logger.debug(f"yaml node {'located in '+file_location+' ' if len(file_location) > 0 else ''}found without version")
         node_version = graph_node.VERSION
 
+    # version is all up to node itself so let node decide if read in version works with loaded node
     is_compatible, warnings = graph_node.check_version_compatibility(node_version)
 
     if is_compatible:
